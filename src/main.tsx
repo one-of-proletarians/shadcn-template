82ff540cf4eacbf2d4707ba10e7e2b4650bdb57f
vitalii <0x1d46@gmail.com>
2024-03-24 16:29:43 +0100

    Add ThemeProvider to main.tsx for global theming

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { ThemeProvider } from "./components/theme-provider";
+
+import "../app/globals.css";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -21,7 +24,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </StrictMode>
   );
 }
